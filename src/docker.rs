cabb31cfeea1428ba601c3ef7abf2ed5ed8e3f7d
Ilya Bygrimov <ilya@occam.fi>
2025-09-01 16:09:22 +0200

    Add clap to control bind address & rustfmt.toml

--- src/docker.rs
+++ src/docker.rs
@@ -1,34 +1,36 @@
-use std::process::Command;
+use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
-use anyhow::{Result, anyhow};
+use std::process::Command;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct DockerContainerStats {
-    pub container: String,
-    pub cpu_perc: String,
-    pub mem_perc: String,
-    pub net_io: String,
+	pub container: String,
+	pub cpu_perc: String,
+	pub mem_perc: String,
+	pub net_io: String,
 }
 
 const DOCKER_FORMAT: &str = r#"{"container": "{{.Name}}", "cpu_perc": "{{.CPUPerc}}", "mem_perc": "{{.MemPerc}}", "net_io": "{{.NetIO}}"}"#;
 
 pub fn stats() -> Result<Vec<DockerContainerStats>> {
-    let output = Command::new("docker")
-        .args(&["stats", "--format", DOCKER_FORMAT, "--no-stream"])
-        .output()?;
+	let output = Command::new("docker")
+		.args(&["stats", "--format", DOCKER_FORMAT, "--no-stream"])
+		.output()?;
 
-    let stdout = String::from_utf8(output.stdout)?;
-    let stderr = String::from_utf8(output.stderr)?;
+	let stdout = String::from_utf8(output.stdout)?;
+	let stderr = String::from_utf8(output.stderr)?;
 
-    if !output.status.success() {
-        eprintln!("`docker stats` returned non-zero exit code with output: \n{}\n{}", stdout, stderr);
-        return Err(anyhow!("Docker stats command did bad :("));
-    }
+	if !output.status.success() {
+		eprintln!(
+			"`docker stats` returned non-zero exit code with output: \n{}\n{}",
+			stdout, stderr
+		);
+		return Err(anyhow!("Docker stats command did bad :("));
+	}
 
-    let json_list_content = stdout.lines().collect::<Vec<&str>>().join(",");
-    let json_string = format!("[{}]", json_list_content);
+	let json_list_content = stdout.lines().collect::<Vec<&str>>().join(",");
+	let json_string = format!("[{}]", json_list_content);
 
-    let result = serde_json::from_str::<Vec<DockerContainerStats>>(json_string.as_str())?;
-    Ok(result)
+	let result = serde_json::from_str::<Vec<DockerContainerStats>>(json_string.as_str())?;
+	Ok(result)
 }
-
--- src/error.rs
+++ src/error.rs
@@ -7,39 +7,35 @@ pub type ApiResult<T, E = ApiError> = Result<T, E>;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ApiError {
-    #[error("an internal server error occurred")]
-    Anyhow(#[from] anyhow::Error),
+	#[error("an internal server error occurred")]
+	Anyhow(#[from] anyhow::Error),
 
-    #[error("Docker error yolo")]
-    Dockworker(#[from] dockworker::errors::Error),
+	#[error("Docker error yolo")]
+	Dockworker(#[from] dockworker::errors::Error),
 }
 
 impl IntoResponse for ApiError {
-    fn into_response(self) -> Response {
-        #[serde_with::serde_as]
-        #[serde_with::skip_serializing_none]
-        #[derive(serde::Serialize)]
-        struct ErrorResponse<'a> {
-            #[serde_as(as = "DisplayFromStr")]
-            message: &'a ApiError,
-        }
+	fn into_response(self) -> Response {
+		#[serde_with::serde_as]
+		#[serde_with::skip_serializing_none]
+		#[derive(serde::Serialize)]
+		struct ErrorResponse<'a> {
+			#[serde_as(as = "DisplayFromStr")]
+			message: &'a ApiError,
+		}
 
-        println!("API error: {self:?}");
+		println!("API error: {self:?}");
 
-        (
-            self.status_code(),
-            Json(ErrorResponse { message: &self }),
-        )
-            .into_response()
-    }
+		(self.status_code(), Json(ErrorResponse { message: &self })).into_response()
+	}
 }
 
 impl ApiError {
-    fn status_code(&self) -> StatusCode {
-        use ApiError::*;
+	fn status_code(&self) -> StatusCode {
+		use ApiError::*;
 
-        match self {
-            Dockworker(_) | Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    }
+		match self {
+			Dockworker(_) | Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
+		}
+	}
 }
