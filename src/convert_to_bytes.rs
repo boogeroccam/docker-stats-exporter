47acda2a77a7f41767d770deafd25953501d7cf8
Ilya Bygrimov <ilya@occam.fi>
2025-09-01 16:43:59 +0200

    Decimal and binary unit support

--- src/convert_to_bytes.rs
+++ src/convert_to_bytes.rs
@@ -5,11 +5,21 @@ use std::collections::HashMap;
 lazy_static! {
 	static ref UNIT_MAP: HashMap<&'static str, f64> = {
 		let mut map = HashMap::new();
+		// Base unit
 		map.insert("B", 1f64);
+
+		// Decimal units (SI)
 		map.insert("kB", 1000f64);
 		map.insert("MB", 1000f64 * 1000f64);
 		map.insert("GB", 1000f64 * 1000f64 * 1000f64);
 		map.insert("TB", 1000f64 * 1000f64 * 1000f64 * 1000f64);
+
+		// Binary units (IEC)
+		map.insert("KiB", 1024f64);
+		map.insert("MiB", 1024f64 * 1024f64);
+		map.insert("GiB", 1024f64 * 1024f64 * 1024f64);
+		map.insert("TiB", 1024f64 * 1024f64 * 1024f64 * 1024f64);
+
 		map
 	};
 }
